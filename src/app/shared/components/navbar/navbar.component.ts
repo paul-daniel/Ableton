b980bdba0973721247be5ebeedd71821fa91e495
paul-daniel <dantchatchouang@gmail.com>
2023-11-18 05:57:20 +0100

    mobile navbar in progress

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
+  isMobile : boolean = false;
+  navMenuOpen : boolean = false;
+
+  ngAfterViewInit(): void {
+    this.checkIsMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event : Event): void {
+    this.checkIsMobile();
+  }
+
+  checkIsMobile() : void {
+    this.isMobile = window.innerWidth <= 850;
+  }
+
+  toggleMenu() {
+    this.navMenuOpen = !this.navMenuOpen;
+  }
 }
