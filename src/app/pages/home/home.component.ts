10becda1692ab768f326640857530fdfe9f3923f
paul-daniel <dantchatchouang@gmail.com>
2023-12-07 09:45:41 +0100

    complex container positioning

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Oriantation } from 'src/app/shared/components/section-portrait/section-portrait.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  LEFT = Oriantation.LEFT;
+  RIGHT = Oriantation.RIGHT;
+  CENTER = Oriantation.CENTER;
 
 }
--- src/app/shared/components/section-portrait/section-portrait.component.ts
+++ src/app/shared/components/section-portrait/section-portrait.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
 
+export enum Oriantation {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+  CENTER = 'CENTER'
+}
+
 @Component({
   selector: 'app-section-portrait',
   templateUrl: './section-portrait.component.html',
@@ -9,4 +15,8 @@ export class SectionPortraitComponent {
   @Input() mainImgUrl: string = '';
   @Input() imagesUrl : string[] = [];
   @Input() color : string = '';
+  @Input() oriantation : Oriantation = Oriantation.LEFT;
+  LEFT = Oriantation.LEFT;
+  RIGHT = Oriantation.RIGHT;
+  CENTER = Oriantation.CENTER;
 }
